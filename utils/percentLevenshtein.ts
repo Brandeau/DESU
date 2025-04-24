b5612af4dfcba32bbfd44302778e339a00365cf8
Brandeau <diego.andrade.y@gmail.com>
2025-04-24 19:34:37 -0400

    feat: create util function percentage levenshtein

---
+++ utils/percentLevenshtein.ts
@@ -0,0 +1,9 @@
+import { levenshtein } from "./levenshtein";
+
+export function percentageLevenshtein(string1: string, string2: string): number{
+    const distance = levenshtein(string1, string2);
+    const bigger = string1.length > string2.length? string1.length : string2.length;
+    const percentage = (bigger - distance) / bigger;
+
+    return percentage * 100;
+}
