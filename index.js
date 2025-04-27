10f0f18888ddbb39886703d3f7d9ff521b6acacd
Brandeau <diego.andrade.y@gmail.com>
2025-04-26 20:52:44 -0400

    refactor

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import express from 'express';
-
-const app = express();
-
 async function getShows(){
 
-    const response = await fetch('https://www.cineplanet.cl/api/cache/sessioncache?subscription-key=c6f97c336b60469189a010a5836fe891');
+    const response = await fetch('https://www.cineplanet.cl/api/cache/sessioncache');
     const data = await response.json();
 
     console.log(data)
