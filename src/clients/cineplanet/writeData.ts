ba35729ff93f17ae48468c93ffc15daa5e7c7bc8
Brandeau <diego.andrade.y@gmail.com>
2025-05-02 13:23:42 -0400

    feat: create functionality to write fetched data into json

---
+++ src/clients/cineplanet/writeData.ts
@@ -0,0 +1,40 @@
+import { getCookies } from "./helpers/getCookies.ts";
+import { parseData } from "./helpers/parseData.ts";
+import { getMovies, getTheatres, getShowings } from "./helpers/fetchData.ts";
+
+import fs from "node:fs";
+
+const subKey: string = process.env.SUBSCRIPTION_KEY;
+const url: string = "https://www.cineplanet.cl";
+const cookie: string = await getCookies(url);
+
+const movies = await getMovies(cookie, url, subKey);
+const theatres = await getTheatres(cookie, url, subKey);
+const showings = await getShowings(cookie, url, subKey);
+
+const parsedMovies = parseData(movies, 'id', 'title', 'cinemas', 'isComingSoon');
+const parsedTheatres = parseData(theatres, 'ID', 'name', 'city');
+
+fs.writeFile("var/movies/movies.json", JSON.stringify(parsedMovies, null, 2), 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return;
+    }
+    console.log('File written successfully!');
+  })
+
+  fs.writeFile("var/theatres/theatres.json", JSON.stringify(parsedTheatres, null, 2), 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return;
+    }
+    console.log('File written successfully!');
+  })
+
+  fs.writeFile("var/showings/showings.json", JSON.stringify(showings, null, 2), 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return;
+    }
+    console.log('File written successfully!');
+  })
