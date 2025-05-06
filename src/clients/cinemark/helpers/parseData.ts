0184604393712c06054b35d038b454666ce10d58
Brandeau <diego.andrade.y@gmail.com>
2025-05-05 22:12:41 -0400

    feat: create parsing function for cinemark

---
+++ src/clients/cinemark/helpers/parseData.ts
@@ -0,0 +1,25 @@
+
+
+export function parseData(list: any[], ...props: string[]): any[]{
+
+    const parsed = [];
+
+    list.forEach(listElem => {
+        const innerObject = {};
+
+        props.forEach(prop => {
+    
+            Object.defineProperty(innerObject, prop, {
+                enumerable: true,
+                configurable: true,
+                writable: true,
+                value: listElem[prop],
+              });
+        });
+
+        parsed.push(innerObject);
+    });
+
+    return parsed;
+}
+
