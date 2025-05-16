83fe711af40bec8f9443196bbb1aedf0a04539db
Brandeau <diego.andrade.y@gmail.com>
2025-05-15 21:08:18 -0400

    feat: add function to fetch movies from Cinemark

--- src/clients/processing.ts
+++ src/clients/processing.ts
@@ -1,8 +1,12 @@
 import { Cineplanet } from "./cineplanet/Cineplanet.ts";
+import { Cinemark } from "./cinemark/Cinemark.ts";
+import { inspect } from "util";
 import { getCookies } from "./cineplanet/helpers/getCookies.ts";
+import { reduceMovies, reduceCinemas, addId } from "./helpers/parse.ts";
 
 const subKey: string = process.env.SUBSCRIPTION_KEY;
 const cineplanet: string = process.env.CINEPLANET_URL;
+const cinemark: string = process.env.CINEMARK_URL;
 const cookie: string = await getCookies(cineplanet);
 
 const theatres = [];
@@ -19,6 +23,7 @@ async function fetchTheatresFromCineplanet(){
 
         theatres.push(x);
     }
+
 }
 
 async function fetchMoviesFromCineplanet(){
@@ -41,5 +46,42 @@ async function fetchShowingsFromCineplanet(){
 
         showings.push(rawShowings['sessions'][i]);
     }
+
+    
 }
 
+async function fetchTheatresFromCinemark(){
+
+    const rawTheatres: any = await Cinemark.getTheatres(cinemark);
+
+    for(let i = 0; i < rawTheatres.length; i++){
+        
+        for(let j = 0; j < rawTheatres[i]['cinemas'].length; j++){
+            const parsed = await Cinemark.parseTheatres(rawTheatres[i]['cinemas'][j])
+
+            theatres.push(parsed)
+        }        
+    }
+}
+
+async function fetchMoviesFromCinemark(){
+
+    const cinema_ids = [2305, 2302, 2306, 2308, 520, 2309, 2301, 2304, 2303, 2300, 517, 2307, 511, 512, 513, 519, 572, 548, 514, 570];
+
+    for(let i = 0; i < cinema_ids.length; i++){
+        const rawMovies = await Cinemark.getBillboard(cinemark, cinema_ids[i]);
+
+        const x = await Cinemark.parseMovies(rawMovies);
+
+        theatres.push(x)
+    }
+
+    const reducedMovies = await reduceMovies(theatres.flat());
+    const reducedCinemas = await reduceCinemas(reducedMovies);
+    const cinemasWithId = await addId(reducedCinemas);
+    
+    console.log(inspect(cinemasWithId, {depth: 11, colors: true}))
+
+}
+
+fetchMoviesFromCinemark();
