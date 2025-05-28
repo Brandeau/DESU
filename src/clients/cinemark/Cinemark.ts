ca49648dd3e7d35fb45eec974e51292a86d81d0a
Brandeau <diego.andrade.y@gmail.com>
2025-05-28 15:20:33 -0400

    feat: create parsing function to filter Cinemark showings

--- src/clients/cinemark/Cinemark.ts
+++ src/clients/cinemark/Cinemark.ts
@@ -44,37 +44,69 @@ export class Cinemark {
 
         const movies = [];
 
-    arr.forEach(element => {
-        element['movies'].forEach(movie => {
-            const idRe = /-[a-zA-Z]+[0-9]+/;
-            const titleRe = /^.+(?=\s\()/;
-    
-            movie['movie_versions'].forEach(version => {
-                const sessions = [];
+        arr.forEach(element => {
+            element['movies'].forEach(movie => {
+                const idRe = /-[a-zA-Z]+[0-9]+/;
+                const titleRe = /^.+(?=\s\()/;
+        
+                movie['movie_versions'].forEach(version => {
+                    const sessions = [];
 
-                version['sessions'].forEach(session => {
-                    sessions.push(session['id'])
+                    version['sessions'].forEach(session => {
+                        sessions.push(session['id'])
+                    });
+
+                    movies.push(
+                        {
+                            "id": movie['film_HO_code'],
+                            "title": version['title'].match(titleRe)[0],
+                            "cinemas": {
+                                "cinemaId": version['id'].split(idRe)[0],
+                                "dates": [{
+                                    "date": element['date'],
+                                    "sessions": sessions
+                                }]
+                            },
+                            "isComingSoon": version['sessions'].length > 0? true : false
+        
+                        }
+                    )
                 });
-
-                movies.push(
-                    {
-                        "id": movie['film_HO_code'],
-                        "title": version['title'].match(titleRe)[0],
-                        "cinemas": {
-                            "cinemaId": version['id'].split(idRe)[0],
-                            "dates": [{
-                                "date": element['date'],
-                                "sessions": sessions
-                            }]
-                        },
-                        "isComingSoon": version['sessions'].length > 0? true : false
-    
-                    }
-                )
             });
         });
-    });
 
         return movies
+    }
+
+    static async parseShowings(arr){
+
+        const showings = [];
+
+        arr.forEach(element => {
+            element['movies'].forEach(movie => {
+        
+                movie['movie_versions'].forEach(version => {
+                    const sessions = [];
+
+                    version['sessions'].forEach(session => {
+                        const re = /(?<=\().*(?=\))/;
+                        const formats = version['title'].match(re) !== null ? version['title'].match(re)[0].split(' ') : []
+                        const lang = formats.pop();
+
+                        showings.push(
+                            {
+                                "id": session['id'],
+                                "showtime": session['showtime'], 
+                                "formats": formats,
+                                "languages": [lang]
+                            }
+                        )
+                    });
+
+                });
+            });
+        });
+
+        return showings;
     }
 }
