3b6ba45bef467bcfb645652c9bdfff9b71dead1a
Brandeau <diego.andrade.y@gmail.com>
2025-06-10 12:18:15 -0400

    style: add eslint styling

--- src/clients/processing.ts
+++ src/clients/processing.ts
@@ -1,143 +1,158 @@
-import { Cineplanet } from "./cineplanet/Cineplanet.ts";
-import { Cinemark } from "./cinemark/Cinemark.ts";
-import { inspect } from "util";
-import { getCookies } from "./cineplanet/helpers/getCookies.ts";
-import { reduceMovies, reduceCinemas, addId, reduceCineplanetShowings } from "./helpers/parse.ts";
 import fs from "node:fs";
 
+import { Cinemark } from "./cinemark/Cinemark.ts";
+import { Cineplanet, type ParsedCineplanetMovie } from "./cineplanet/Cineplanet.ts";
+import { getCookies } from "./cineplanet/helpers/getCookies.ts";
+import { processMovies, reduceCineplanetShowings } from "./helpers/parse.ts";
 
 const subKey: string = process.env.SUBSCRIPTION_KEY;
 const cineplanet: string = process.env.CINEPLANET_URL;
 const cinemark: string = process.env.CINEMARK_URL;
 const cookie: string = await getCookies(cineplanet);
 
-const theatres = [];
-const movies = [];
-const showings = [];
+async function fetchTheatresFromCineplanet() {
+  const rawTheatres = await Cineplanet.getTheatres(cookie, cineplanet, subKey);
+  const theatres = [];
 
-async function fetchTheatresFromCineplanet(){
+  for (let i = 0; i < rawTheatres["cinemas"].length; i++) {
+    const parsed = await Cineplanet.parseTheatres(rawTheatres["cinemas"][i]);
 
-    const rawTheatres = await Cineplanet.getTheatres(cookie, cineplanet, subKey);
+    theatres.push(parsed);
+  }
 
-    for(let i = 0; i < rawTheatres['cinemas'].length; i++){
-        
-        const parsed = await Cineplanet.parseTheatres(rawTheatres['cinemas'][i]);
-
-        theatres.push(parsed);
-    }
-
+  return theatres;
 }
 
-async function fetchMoviesFromCineplanet(){
+async function fetchMoviesFromCineplanet() {
+  const rawMovies = await Cineplanet.getMovies(cookie, cineplanet, subKey);
+  const movies = [];
 
-    const rawMovies = await Cineplanet.getMovies(cookie, cineplanet, subKey);
-    const films = [];
+  for (let i = 0; i < rawMovies["movies"].length; i++) {
+    const parsed = await Cineplanet.parseMovies(rawMovies["movies"][i]);
 
-    for(let i = 0; i < rawMovies['movies'].length; i++){
-        
-        const parsed = await Cineplanet.parseMovies(rawMovies['movies'][i]);
+    movies.push(parsed);
+  }
 
-        films.push(parsed);
-    }
+  const reducedMovies = await reduceCineplanetShowings(movies as ParsedCineplanetMovie[]);
 
-    const reducedFilms = await reduceCineplanetShowings(films.flat());
-
-    //@ts-ignore
-    await reducedFilms.forEach(cinema => {
-        movies.push(cinema);
-    })
+  return reducedMovies;
 }
 
-async function fetchShowingsFromCineplanet(){
+async function fetchShowingsFromCineplanet() {
+  const rawShowings = await Cineplanet.getShowings(cookie, cineplanet, subKey);
+  const showings = [];
 
-    const rawShowings = await Cineplanet.getShowings(cookie, cineplanet, subKey);
+  for (let i = 0; i < rawShowings["sessions"].length; i++) {
+    showings.push(rawShowings["sessions"][i]);
+  }
 
-    for(let i = 0; i < rawShowings['sessions'].length; i++){
-
-        showings.push(rawShowings['sessions'][i]);
-    }
-    
+  return showings;
 }
 
-async function fetchTheatresFromCinemark(){
+async function fetchTheatresFromCinemark() {
+  const rawTheatres = await Cinemark.getTheatres(cinemark);
+  const theatres = [];
 
-    const rawTheatres = await Cinemark.getTheatres(cinemark);
+  for (let i = 0; i < rawTheatres.length; i++) {
+    for (let j = 0; j < rawTheatres[i]["cinemas"].length; j++) {
+      const parsed = await Cinemark.parseTheatres(rawTheatres[i]["cinemas"][j]);
 
-    for(let i = 0; i < rawTheatres.length; i++){
-        
-        for(let j = 0; j < rawTheatres[i]['cinemas'].length; j++){
-            const parsed = await Cinemark.parseTheatres(rawTheatres[i]['cinemas'][j])
-
-            theatres.push(parsed)
-        }        
+      theatres.push(parsed);
     }
+  }
+
+  return theatres;
 }
 
-async function fetchMoviesFromCinemark(){
+async function fetchMoviesFromCinemark() {
+  const cinema_ids = [
+    2305, 2302, 2306, 2308, 520, 2309, 2301, 2304, 2303, 2300, 517, 2307, 511, 512, 513,
+    519, 572, 548, 514, 570,
+  ];
+  const movies = [];
 
-    const cinema_ids = [2305, 2302, 2306, 2308, 520, 2309, 2301, 2304, 2303, 2300, 517, 2307, 511, 512, 513, 519, 572, 548, 514, 570];
-    const films = [];
+  for (let i = 0; i < cinema_ids.length; i++) {
+    const rawMovies = await Cinemark.getBillboard(cinemark, cinema_ids[i]);
 
-    for(let i = 0; i < cinema_ids.length; i++){
-        const rawMovies = await Cinemark.getBillboard(cinemark, cinema_ids[i]);
+    const parsed = await Cinemark.parseMovies(rawMovies);
 
-        const parsed = await Cinemark.parseMovies(rawMovies);
+    movies.push(parsed);
+  }
+  const reducedMovies = await processMovies(movies.flat());
 
-        films.push(parsed)
-    }
-    const reducedMovies = await reduceMovies(films.flat());
-    const reducedCinemas = await reduceCinemas(reducedMovies);
-    const cinemasWithId = await addId(reducedCinemas);
-    
-    await cinemasWithId.forEach(cinema => {
-        movies.push(cinema);
-    })
+  return reducedMovies;
 }
 
-async function fetchShowingsFromCinemark(){
+async function fetchShowingsFromCinemark() {
+  const cinema_ids = [
+    2305, 2302, 2306, 2308, 520, 2309, 2301, 2304, 2303, 2300, 517, 2307, 511, 512, 513,
+    519, 572, 548, 514, 570,
+  ];
+  const showings = [];
 
-    const cinema_ids = [2305, 2302, 2306, 2308, 520, 2309, 2301, 2304, 2303, 2300, 517, 2307, 511, 512, 513, 519, 572, 548, 514, 570];
+  for (let i = 0; i < cinema_ids.length; i++) {
+    const rawMovies = await Cinemark.getBillboard(cinemark, cinema_ids[i]);
 
-    for(let i = 0; i < cinema_ids.length; i++){
-        const rawMovies = await Cinemark.getBillboard(cinemark, cinema_ids[i]);
+    const parsed = await Cinemark.parseShowings(rawMovies);
 
-        const parsed = await Cinemark.parseShowings(rawMovies);
+    showings.push(parsed);
+  }
 
-        showings.push(parsed)
-    }
+  return showings;
 }
 
-async function handler(){
-    await fetchTheatresFromCinemark();
-    await fetchTheatresFromCineplanet();
-    await fetchMoviesFromCinemark();
-    await fetchMoviesFromCineplanet();
-    await fetchShowingsFromCinemark();
-    await fetchShowingsFromCineplanet();
+async function handler() {
+  const cinemarkTheatres = await fetchTheatresFromCinemark();
+  const cineplanetTheatres = await fetchTheatresFromCineplanet();
+  const cinemarkMovies = await fetchMoviesFromCinemark();
+  const cineplanetMovies = await fetchMoviesFromCineplanet();
+  const cinemarkShowings = await fetchShowingsFromCinemark();
+  const cineplanetShowings = await fetchShowingsFromCineplanet();
 
-    await fs.writeFile("var/data/theatres.json", JSON.stringify(theatres, null, 2), 'utf8', (err) => {
-        if (err) {
-          console.error('Error writing file:', err);
-          return;
-        }
-        console.log('File written successfully!');
-    });
+  const theatres = [...cineplanetTheatres, ...cinemarkTheatres];
 
-    await fs.writeFile("var/data/movies.json", JSON.stringify(movies.flat(), null, 2), 'utf8', (err) => {
-        if (err) {
-          console.error('Error writing file:', err);
-          return;
-        }
-        console.log('File written successfully!');
-    });
+  await fs.writeFile(
+    "var/data/theatres.json",
+    JSON.stringify(theatres, null, 2),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return;
+      }
+      console.log("File written successfully!");
+    },
+  );
 
-    await fs.writeFile("var/data/showings.json", JSON.stringify(showings.flat(), null, 2), 'utf8', (err) => {
-        if (err) {
-          console.error('Error writing file:', err);
-          return;
-        }
-        console.log('File written successfully!');
-    });
+  const movies = [...cineplanetMovies, ...cinemarkMovies];
+
+  await fs.writeFile(
+    "var/data/movies.json",
+    JSON.stringify(movies, null, 2),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return;
+      }
+      console.log("File written successfully!");
+    },
+  );
+
+  const showings = [...cineplanetShowings, ...cinemarkShowings];
+
+  await fs.writeFile(
+    "var/data/showings.json",
+    JSON.stringify(showings, null, 2),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return;
+      }
+      console.log("File written successfully!");
+    },
+  );
 }
 
 handler();
