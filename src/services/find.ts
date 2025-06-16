1de48cf96269bf893a1df48dec720ad31b074615
Brandeau <diego.andrade.y@gmail.com>
2025-06-16 13:04:56 -0400

    feat: create function to filter movies from title

--- src/services/find.ts
+++ src/services/find.ts
@@ -1,6 +1,6 @@
 import { type Movie } from "../clients/cinemark/fetchData.ts";
 
-async function getMovies(): Promise<Movie[]> {
+async function fetchMovies(): Promise<Movie[]> {
   try {
     const response = await fetch("../../var/data/movies.json");
     if (!response.ok) {
@@ -13,3 +13,16 @@ async function getMovies(): Promise<Movie[]> {
     throw Error("Error loading movies", error);
   }
 }
+
+async function filterMovies(title: string): Promise<Movie[]> {
+  const movies = await fetchMovies();
+  try {
+    const movie = movies.filter((movie) => movie.title === title);
+
+    return movie;
+  } catch (error) {
+    throw Error("No movies with that name", error);
+  }
+}
+
+console.log(filterMovies("BAILARINA"));
