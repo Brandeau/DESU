b356551aca30698177761463b3c9ae1ec50ca4f1
Brandeau <diego.andrade.y@gmail.com>
2025-07-01 18:47:34 -0400

    refactor: made getCookies function a method of the Cineplanet class

--- src/clients/cineplanet/getCookies.ts
+++
@@ -1,6 +0,0 @@
-export async function getCookies(url: string): Promise<string> {
-  const response = await fetch(url);
-  const cookie = response.headers.getSetCookie()[0].split(";")[0];
-
-  return cookie;
-}
