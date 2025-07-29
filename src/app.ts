d4926fe5f239d938964ac547368de6e2364036ff
Brandeau <diego.andrade.y@gmail.com>
2025-07-29 14:50:07 -0400

    feat: create command
    Build a basic command for executing the search of movies from the list

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import { Command } from "@cliffy/command";
+
+import { searchMovieByTitle } from "./services/find.ts";
+
+new Command()
+  .name("desu")
+  .version("0.1.0")
+  .description(
+    "Command line application to fetch all showings for a given movie across all main theatre chains in Santiago, Chile",
+  )
+  .command(
+    "search <title:string> [city:string]",
+    "Find all available showings of a movie",
+  )
+  .action((options, title) => {
+    const results = searchMovieByTitle(title);
+
+    console.log(results);
+  })
+  .parse(process.argv.slice(2));
