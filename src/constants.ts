41285a580ad3e8cce2ec67ff8618157ed29e7af0
Brandeau <diego.andrade.y@gmail.com>
2025-08-13 21:02:23 -0400

    fix: mixed up urls
    Corrected a mix up in the urls of two cinema chains

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const CINEMA_CHAIN = Object.freeze({
 });
 
 export const URLS = Object.freeze({
-  CINEPLANET: "aHR0cHM6Ly9jaW5lcG9saXNjaGlsZS5jbA",
+  CINEPLANET: "aHR0cHM6Ly93d3cuY2luZXBsYW5ldC5jbA",
   CINEMARK: "aHR0cHM6Ly9hcGkuY2luZW1hcmsuY2w",
-  CINEPOLIS: "aHR0cHM6Ly93d3cuY2luZXBsYW5ldC5jbA",
+  CINEPOLIS: "aHR0cHM6Ly9jaW5lcG9saXNjaGlsZS5jbA",
 });
